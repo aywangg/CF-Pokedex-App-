f4ee63ac6913fa59e1ee7e529c2d39919e19cefd
aywangg <wangaustin528@gmail.com>
2022-11-14 15:27:19 -0800

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ let pokemonList= [
   {name: "Venusaur", type: ["poison", "grass"], height: "2"}
   {name: "Dragonite", type: ["dragon", "flying"], height: "2.2"}
  ]
+
+ for (let i=0; i < pokemonList.length; i++)
+ {
+  if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That's as tall as a baseball bat!" + "<br>")
+  }else if (pokemonList[i].height < 2){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That's as tall as Michael Jordan!" + "<br>")
+  }else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Wow, that is tall!" + "<br>")
+  }
+ }

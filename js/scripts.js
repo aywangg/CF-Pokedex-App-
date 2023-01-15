b708db69c2ef6b0b163f504a700bd88968a3bed4
aywangg <wangaustin528@gmail.com>
2023-01-14 16:06:53 -0800

    added forEach func

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,34 @@ let pokemonList= [
   {name: "Charzard", type: ["flying", "fire"], height: "1.7"}
   {name: "Venusaur", type: ["poison", "grass"], height: "2"}
   {name: "Dragonite", type: ["dragon", "flying"], height: "2.2"}
- ]
+ ];
 
+function myLoop(list){
+  document.write(list.name = " - Height: " + list.height + "<br>");
+}
+pokemonList.forEach(myLoop);
+
+{
+  if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That's as tall as a baseball bat!" + "<br>")
+  }else if (pokemonList[i].height < 2){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That's as tall as Michael Jordan!" + "<br>")
+  }else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Wow, that is tall!" + "<br>")
+  }
+ }
+
+ function printArrayDetails(list){
+  for (let i=0; i < list.length; i++){
+    document.write("<p>" + list[i].name + "</p>")
+    console.log(list[i].name);
+  }
+ }
+
+ printArrayDetails(pokemonstList);
+ 
+ /*
+                                                      For Loop Code
  for (let i=0; i < pokemonList.length; i++)
  {
   if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1){
@@ -14,3 +40,5 @@ let pokemonList= [
     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Wow, that is tall!" + "<br>")
   }
  }
+ */
+

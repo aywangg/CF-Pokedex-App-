c7b2a54bc514133e46227c55c93ea1af49e843c9
aywangg <wangaustin528@gmail.com>
2023-01-14 16:52:28 -0800

    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
+let pokemonRepository = (function () {
+
 let pokemonList= [
   {name: "Charzard", type: ["flying", "fire"], height: "1.7"}
   {name: "Venusaur", type: ["poison", "grass"], height: "2"}
   {name: "Dragonite", type: ["dragon", "flying"], height: "2.2"}
- ];
+ ]
 
-function myLoop(list){
-  document.write(list.name = " - Height: " + list.height + "<br>");
-}
-pokemonList.forEach(myLoop);
-
-{
+ {
   if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1){
     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That's as tall as a baseball bat!" + "<br>")
   }else if (pokemonList[i].height < 2){
@@ -19,15 +16,27 @@ pokemonList.forEach(myLoop);
   }
  }
 
- function printArrayDetails(list){
-  for (let i=0; i < list.length; i++){
-    document.write("<p>" + list[i].name + "</p>")
-    console.log(list[i].name);
-  }
- }
+function myLoop(list){
+  document.write(list.name = " - Height: " + list.height + "<br>");
+}
 
- printArrayDetails(pokemonstList);
- 
+pokemonList.forEach(myLoop);
+
+function getAll () {
+  return pokemonList;
+}
+function add (pokemon) {
+  pokemonList.push(pokemon)
+}
+
+return {
+  getAll: getAll,
+  add: add
+}
+
+})()
+
+
  /*
                                                       For Loop Code
  for (let i=0; i < pokemonList.length; i++)

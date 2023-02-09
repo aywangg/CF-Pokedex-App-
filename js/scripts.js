3284465e31c7b62b4512a947219b1a84809028ec
aywangg <wangaustin528@gmail.com>
2023-02-08 16:25:05 -0800

    reminified scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -42,76 +42,106 @@ let pokemonRepository = (function () {
     });
   }
 
-    listItem.append(button);
-    pokemonListElement.append(listItem);
-    
-          button.on('click', function() {
-        showDetails(pokemon);
-      });   
-  }
-
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
       });
-    }).catch(function (e) {
-      console.error(e);
-    })
   }
 
   function loadDetails(item) {
-    
     let url = item.detailsUrl;
-    
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageFront = details.sprites.front_default;
+        item.imageBack = details.sprites.back_default;
+        item.height = details.height;
+        item.weight = details.weight;
+        item.types = details.types;
+        item.abilities = details.abilities;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
 
-  function showDetails(pokemon){
-    loadDetails(pokemon).then(function(){
-      showDetailsModal(pokemon);
-    });
-  }
-
-  function showDetailsModal(pokemon) {
-    let modalBody = $('.modal-body');
+function showModal (pokemon){
+  loadDetails(pokemon).then(function (){
     let modalTitle = $('.modal-title');
+    modalTitle.empty();
+    let pokemonCapped = 
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    modalTitle.append(pokeCapped);
 
+    let modalBody = $('.modal-body');
     modalBody.empty();
-    modalTitle.text(pokemon.name);
+    modalBody.append(
+      `<img class="modal-image" src="${pokemon.imageFront}">`
+    );
+    modalBody.append(
+      `<img class="modal-image" src="${pokemon.imageBack}">`
+    );
+    modalBody.append(`<p>Height: ${pokemon.height}</p>`);
+    modalBody.append(`<p>Weight: ${pokemon.weight}</p>`);
 
-    let height = $('<p>' + 'Height:  ' + pokemon.height + '</p>');
-    let image = $('<img class="pokemon-img" src="' + pokemon.imageUrl + '" />');
-    let types = $('<p>' + 'Types:  ' + pokemon.types + '</p>');
-    let abilities = $('<p>' + 'Abilities: ' + pokemon.abilities + '</p>');
-      
-    modalBody.append(image);
-    modalBody.append(height);
-    modalBody.append(types);
-    modalBody.append(abilities);
+    let types = pokemon.types;
+    let typesList = '';
+    for (let i = 0; i < types.length; i++) {
+      if (i < types.length - 1) {
+        typesList += types[i].type.name + ', ';
+      } else {
+        typesList += types[i].type.name;
+      }
+    }
+    modalBody.append(`<p>Types: ${typesList}</p>`);
 
+    let abilities = pokemon.abilities;
+    let abilityList = '';
+    for (let i = 0; i < abilities.length; i++) {
+      if (i < abilities.length - 1) {
+        abilityList += abilities[i].ability.name + ', ';
+      } else {
+        abilityList += abilities[i].ability.name;
+      }
+    }
+    modalBody.append(`<p>Abilities: ${abilityList}</p>`);
+  });
+}
+
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+  let modalContainer = document.querySelector('#modal-container');
+  if (
+    e.key === 'Escape' &&
+    modalContainer.classList.contains('is-visible')
+  ) {
+    hideModal();
   }
+});
 
 return {
   getAll: getAll,
   add: add,
   addListItem: addListItem,
-  showDetails: showDetails,
   loadList: loadList,
   loadDetails: loadDetails,
   showDetailsModal: showDetailsModal
